6f59e77bc4c7a9219c517534e288e89946e3b9d7
Jhonatas Rodrigues Correa <jhonatasrcorrea@gmail.com>
2022-10-07 18:08:18 -0300

    Adicionar arquivos de projeto.

---
+++ OngAdocoes2.0/Animal.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngAdocoes2._0
+{
+    internal class Animal
+    {
+        public int Chip { get; set; }
+        public string Familia { get; set; }
+        public string Raca { get; set; }
+        public string Sexo { get; set; }
+        public string Nome { get; set; }
+        public Animal()
+        {
+
+        }
+        public Animal(int chip, string familia, string raca, string sexo, string nome)
+        {
+            Chip = chip;
+            Familia = familia;
+            Raca = raca;
+            Sexo = sexo;
+            Nome = nome;
+        }
+        public Animal(int chip, string familia, string raca, string sexo)
+        {
+            Chip = chip;
+            Familia = familia;
+            Raca = raca;
+            Sexo = sexo;
+            Nome = "SemNome";
+        }
+
+        public Animal CadastroAnimal()
+        {
+            int chip = NumeroRandom();
+            Console.WriteLine("informe a classificação de familia deste animal (Gato, Cachorro, Peixe...): ");
+            string familia = Console.ReadLine();
+            while (familia == "\n")
+            {
+                Console.WriteLine("Valor inválido inserido, informe novamente:");
+                familia = Console.ReadLine();
+            }
+            Console.WriteLine("Informe a raça do animal: ");
+            string raca = Console.ReadLine();
+            Console.WriteLine("Informe o sexo do animal (M ou F):");
+            string sexo = Console.ReadLine().ToUpper();
+            while (sexo != "M" && sexo != "F")
+            {
+                Console.WriteLine("Opção inválida, informe novamente o valor: ");
+                sexo = Console.ReadLine().ToUpper();
+            }
+            Console.WriteLine("Deseja informar o nome do animal: \n[1]Sim \n[2]Não");
+            int opc = int.Parse(Console.ReadLine());
+            while (opc != 1 && opc != 2)
+            {
+                Console.WriteLine("Opção inválida, informe novamente o valor: ");
+                opc = int.Parse(Console.ReadLine());
+            }
+            if (opc == 1)
+            {
+                Console.WriteLine("Informe o Nome do animal: ");
+                string nome = Console.ReadLine();
+                Animal animal = new Animal(chip, familia, raca, sexo, nome);
+                Console.WriteLine("Cadastro finalizado, animal cadastrado em nosso banco!");
+                Console.WriteLine(animal.ToString());
+                Console.WriteLine("Pressione enter para continuar...");
+                Console.ReadKey();
+                return animal;
+            }
+            else
+            {
+                Animal animal = new Animal(chip, familia, raca, sexo);
+                Console.WriteLine("Cadastro finalizado, animal cadastrado em nosso banco!");
+                Console.WriteLine(animal.ToString());
+                Console.WriteLine("Pressione enter para continuar...");
+                Console.ReadKey();
+                return animal;
+            }
+        }
+        public static int NumeroRandom()
+        {
+            Random rand = new Random();
+            int[] numero = new int[100];
+            int aux = 0;
+            for (int k = 0; k < numero.Length; k++)
+            {
+                int rnd = 0;
+                do
+                {
+                    rnd = rand.Next(100, 999);
+                } while (numero.Contains(rnd));
+                numero[k] = rnd;
+                aux = numero[k];
+            }
+            return aux;
+        }
+        public override string ToString()
+        {
+            return "\nNumero Identificação (CHIP): " + Chip +
+                "\nNome do animal: " + Nome +
+                "\nFamilia: " + Familia +
+                "\nRaca: " + Raca + 
+                "\nSexo: " + Sexo;
+        }
+    }
+}
---
+++ OngAdocoes2.0/Pessoa.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngAdocoes2._0
+{
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Sexo { get; set; }
+        public string DataNascimento { get; set; }
+        public string Telefone { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string SiglaEstado { get; set; }
+        public string Numero { get; set; }
+        public Pessoa()
+        {
+
+        }
+        public Pessoa(string nome, string cPF, string sexo, string dataNascimento, string telefone, string rua, string bairro, string cidade, string siglaEstado, string numero)
+        {
+            Nome = nome;
+            CPF = cPF;
+            Sexo = sexo;
+            DataNascimento = dataNascimento;
+            Telefone = telefone;
+            Logradouro = rua;
+            Bairro = bairro;
+            Cidade = cidade;
+            SiglaEstado = siglaEstado;
+            Numero = numero;
+        }
+        public Pessoa CadastroPessoa()
+        {
+            Console.Write("Insira seu nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Insira seu CPF (XXX.XXX.XXX-XX): ");
+            string cpf = Console.ReadLine();
+            Console.Write("Insira seu Sexo (M ou F): ");
+            string sexo = Console.ReadLine().ToUpper();
+            while (sexo != "M" && sexo != "F")
+            {
+                Console.Write("Valor incorreto informado, informe novamente: ");
+                sexo = Console.ReadLine().ToUpper();
+            }
+            Console.Write("Insira sua data de nascimento (dd/mm/aaaa): ");
+            string dataNascimento = Console.ReadLine();
+            Console.Write("Insira seu telefone ((xx) xxxxx-xxxx): ");
+            string telefone = Console.ReadLine();
+            Console.Write("Insira seu Logradouro: ");
+            string logradouro = Console.ReadLine();
+            Console.Write("Insira seu bairro: ");
+            string bairro = Console.ReadLine();
+            Console.Write("Insira sua cidade: ");
+            string cidade = Console.ReadLine();
+            Console.Write("Insira a sigla do estado em que reside: ");
+            string siglaEstado = Console.ReadLine();
+            Console.Write("Insira o numero da casa: ");
+            string numero = Console.ReadLine();
+
+            Pessoa novaPessoa = new Pessoa(nome, cpf, sexo, dataNascimento, telefone, logradouro, bairro, cidade, siglaEstado, numero);
+            Console.WriteLine("Dados Obtidos: ");
+            Console.WriteLine("---------------");
+            Console.WriteLine(novaPessoa.ToString());
+            Console.WriteLine("Pressione uma tecla para continuar...");
+            Console.ReadKey();
+            return novaPessoa;
+        }
+        public override string ToString()
+        {
+            return
+                "\nNome: " + Nome +
+                "\nCPF: " + CPF +
+                "\nSexo: " + Sexo +
+                "\nData de Nascimento: " + DataNascimento +
+                "\nTelefone: " + Telefone +
+                "\nEstado: " + SiglaEstado;
+        }
+    }
+}
+
